01b70c6d497b9e3e70aab6973ead7b1bd95d8e1c
Alyson Knowles <aknowles18@kcimia.com>
2020-12-28 12:28:15 -0500

    change nano 33 ble sense to central & add on/off

--- magic_wand.ino
+++ magic_wand.ino
@@ -52,6 +52,8 @@ BLEService magicWandService("33e8c28d-f16c-46aa-8d77-85608274807f");
 // BLE Magic Wand Characteristic
 BLEUnsignedCharCharacteristic magicWandGestureChar("155b8a1f-e774-4556-b01b-6723bddb1d65",  // custom 128-bit characteristic UUID
     BLERead | BLENotify); // remote clients will be able to get notifications if this characteristic changes
+// Philips Hue Turn off/on
+bool isLumosActive = false;
 
 // The name of this function is important for Arduino compatibility.
 void setup() {
@@ -117,29 +119,38 @@ void setup() {
     error_reporter->Report("Set up failed\n");
   }
 
-  // Setup for BLE
-  Serial.begin(9600);    // initialize serial communication
-  //  while (!Serial);
-  // begin initialization
-  if (!BLE.begin()) {
-    Serial.println("starting BLE failed!");
-    while (1);
-  }
-  /* Set a local name for the BLE device
-     This name will appear in advertising packets
-     and can be used by remote devices to identify this BLE device
-     The name can be changed but maybe be truncated based on space left in advertisement packet
-  */
-  BLE.setLocalName("MagicWand");
-  BLE.setAdvertisedService(magicWandService); // add the service UUID
-  magicWandService.addCharacteristic(magicWandGestureChar); // add the magic wand gesture characteristic
-  BLE.addService(magicWandService); // Add the magic wand service
-  /* Start advertising BLE.  It will start continuously transmitting BLE
-     advertising packets and will be visible to remote BLE central devices
-     until it receives a new connection */
-  // start advertising
-  BLE.advertise();
-  Serial.println("Bluetooth device active, waiting for connections...");
+  //  // Setup for BLE peripheral
+  //  Serial.begin(9600);    // initialize serial communication
+  //  // begin initialization
+  //  if (!BLE.begin()) {
+  //    Serial.println("starting BLE failed!");
+  //    while (1);
+  //  }
+  //  /* Set a local name for the BLE device
+  //     This name will appear in advertising packets
+  //     and can be used by remote devices to identify this BLE device
+  //     The name can be changed but maybe be truncated based on space left in advertisement packet
+  //  */
+  //  BLE.setLocalName("MagicWand");
+  //  BLE.setAdvertisedService(magicWandService); // add the service UUID
+  //  magicWandService.addCharacteristic(magicWandGestureChar); // add the magic wand gesture characteristic
+  //  BLE.addService(magicWandService); // Add the magic wand service
+  //  /* Start advertising BLE.  It will start continuously transmitting BLE
+  //     advertising packets and will be visible to remote BLE central devices
+  //     until it receives a new connection */
+  //  // start advertising
+  //  BLE.advertise();
+  //  Serial.println("Bluetooth device active, waiting for connections...");
+
+  // Setup for BLE Central
+  Serial.begin(9600);
+  // configure the button pin as input
+//  pinMode(buttonPin, INPUT);
+  // initialize the BLE hardware
+  BLE.begin();
+  Serial.println("BLE Central - Magic Wandcontrol");
+  // start scanning for peripherals
+  BLE.scanForUuid("932c32bd-0000-47a2-835a-a8d455b859dd");
 }
 
 void loop() {
@@ -160,21 +171,100 @@ void loop() {
   int gesture_index = PredictGesture(interpreter->output(0)->data.f);
   // Clear the buffer next time we read data
   should_clear_buffer = gesture_index < 3;
-  // wait for a BLE central
-  BLEDevice central = BLE.central();
-  // if a central is connected to the peripheral:
-  if (central && should_clear_buffer) {
-    Serial.print("Connected to central: ");
-    // print the central's BT address:
-    Serial.println(central.address());
-    Serial.print("Magic Wand Gesture is now: "); // print it
-    Serial.println(gesture_index);
-    magicWandGestureChar.writeValue(gesture_index);  // and update the magic wand gesture characteristic
+  //  BLE Peripheral
+  //   wait for a BLE central
+  //  BLEDevice central = BLE.central();
+  //  // if a central is connected to the peripheral:
+  //  if (central && should_clear_buffer) {
+  //    Serial.print("Connected to central: ");
+  //    // print the central's BT address:
+  //    Serial.println(central.address());
+  //    Serial.print("Magic Wand Gesture is now: "); // print it
+  //    Serial.println(gesture_index);
+  //    magicWandGestureChar.writeValue(gesture_index);  // and update the magic wand gesture characteristic
+  //  }
+  //  if (!central.connected()) {
+  //    Serial.print("Disconnected from central: ");
+  //    Serial.println(central.address());
+  //  }
+  //  // Produce an output
+  //  HandleOutput(error_reporter, gesture_index);
+
+  // BLE Central
+  // check if a peripheral has been discovered
+  BLEDevice peripheral = BLE.available();
+  if (peripheral) {
+    // discovered a peripheral, print out address, local name, and advertised service
+    Serial.print("Found ");
+    Serial.print(peripheral.address());
+    Serial.print(" '");
+    Serial.print(peripheral.localName());
+    Serial.print("' ");
+    Serial.print(peripheral.advertisedServiceUuid());
+    Serial.println();
+    if (peripheral.localName() != "Hue Lamp") {
+      return;
+    }
+    // stop scanning
+    BLE.stopScan();
+    if(should_clear_buffer) {
+       controlHueLumos(peripheral);
+    }
+    // peripheral disconnected, start scanning again
+    BLE.scanForUuid("932c32bd-0000-47a2-835a-a8d455b859dd");
   }
-//  if (!central.connected()) {
-//    Serial.print("Disconnected from central: ");
-//    Serial.println(central.address());
-//  }
   // Produce an output
   HandleOutput(error_reporter, gesture_index);
+}
+
+void controlHueLumos(BLEDevice peripheral) {
+  // connect to the peripheral
+  Serial.println("Connecting ...");
+  if (peripheral.connect()) {
+    Serial.println("Connected");
+  } else {
+    Serial.println("Failed to connect!");
+    return;
+  }
+  // discover peripheral attributes
+  Serial.println("Discovering attributes ...");
+  if (peripheral.discoverAttributes()) {
+    Serial.println("Attributes discovered");
+  } else {
+    Serial.println("Attribute discovery failed!");
+    peripheral.disconnect();
+    return;
+  }
+  // retrieve the LED characteristic
+  BLECharacteristic ledCharacteristic = peripheral.characteristic("932c32bd-0002-47a2-835a-a8d455b859dd");
+  if (!ledCharacteristic) {
+    Serial.println("Peripheral does not have On/Off characteristic!");
+    peripheral.disconnect();
+    return;
+  } else if (!ledCharacteristic.canWrite()) {
+    Serial.println("Peripheral does not have a writable On/Off characteristic!");
+    peripheral.disconnect();
+    return;
+  }
+  while (peripheral.connected()) {
+    // while the peripheral is connected
+    // read the button pin
+    //    int buttonState = digitalRead(buttonPin);
+    //    if (oldButtonState != buttonState) {
+    // button changed
+    //      oldButtonState = buttonState;
+    if (isLumosActive) {
+      Serial.println("lumos on");
+      // button is pressed, write 0x01 to turn the LED on
+      ledCharacteristic.writeValue((byte)0x01);
+      isLumosActive = true;
+    } else {
+      Serial.println("lumos off");
+      // button is released, write 0x00 to turn the LED off
+      ledCharacteristic.writeValue((byte)0x00);
+      isLumosActive = false;
+    }
+    //  }
+  }
+  Serial.println("Peripheral disconnected");
 }
